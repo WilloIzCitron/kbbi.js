b401c4a13d21a1477569de1a76e19e74b277ddc1
Tegas Aziz <49786146+WilloIzCitron@users.noreply.github.com>
2025-01-11 10:38:42 +0700

    puppeteer ke cheerio
    fungsionalitas susah kalo buat VPS

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const puppeteer = require('puppeteer');
 const { parse } = require('node-html-parser');
+const cheerio = require('cheerio');
 
 async function cari(keyword) {
     if(!keyword) throw new Error('Please provide any keyword to find!');
-    //to initiate puppeteer
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+    const $ = await cheerio.fromURL(`https://kbbi.kemdikbud.go.id/entri/${keyword}`)
 
-    await page.goto(`https://kbbi.kemdikbud.go.id/entri/${keyword}`);
-    const res = await page.content()
-    const root = parse(res);
+    const root = parse($.html());
     const lema = root.querySelector('h2').text;
     let arti = root.querySelectorAll('ol li').map(x => x.text.slice(1).split("  ").join(""));
     if (arti.length === 0) {
@@ -20,8 +17,9 @@ async function cari(keyword) {
     if (arti.length === 0) {
         return { lema: null, arti: null};
     }
-    await browser.close();
     return { lema, arti};
 }
+
+cari('dilema').then(console.log)
 
 module.exports = { cari };
